5d155f5d9fbeab703aeff3f70a4375e0e68ae93e
Iryusha <Sherbinina_Irina@ukr.net>
2022-02-21 19:08:31 +0200

    task1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
-// import { fetchCountries } from './js/fetchCountries';
+
 import { countryTemplate, countryMinTemplate } from './js/countryTemplate';
 
 const DEBOUNCE_DELAY = 300;
